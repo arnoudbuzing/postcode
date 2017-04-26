0780fa26b7798891f020046d8d6c14d4dcc4dd59
Arnoud Buzing <arnoudb@wolfram.com>
2017-04-26 10:17:11 -0500

    add lat/lon support

--- postcode/postcode.m
+++ postcode/postcode.m
@@ -10,6 +10,25 @@ postcode[code_String] := Module[{ request, response },
  ImportString[response["Body"], "RawJSON"] 
 ]
 
+postcode[codes_List] := Module[{ request, response },
+ request = HTTPRequest[
+  "http://api.postcodes.io/postcodes", <|
+   Method -> "POST",
+   "Body" -> {"postcodes" -> codes},
+   "Headers" -> {"content-type" -> "application/x-www-form-urlencoded;charset=UTF-8", "user-agent" -> "Wolfram HTTPClient 11.1"}
+  |>
+ ];
+ response = URLRead[request];
+ ImportString[response["Body"], "RawJSON"]
+]
+
+postcode[location_GeoPosition] := Module[ {request,response,lat,lon},
+ {lat, lon} = ToString /@ QuantityMagnitude[LatitudeLongitude[location]];
+ request = HTTPRequest[ "http://api.postcodes.io/postcodes?lon=" <> lon <> "&lat=" <> lat];
+ response = URLRead[request];
+ ImportString[response["Body"], "RawJSON"]
+]
+
 End[]
 
 EndPackage[]
